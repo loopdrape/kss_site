eaf32599d5e5976594816fa1d6e7a7aa033857ce
loopdrape <loopdrape@me.com>
2017-01-27 19:05:59 +0900

    update wAB.js

--- js/webAppBase-1.1.3.js
+++ js/webAppBase-1.1.3.js
@@ -236,6 +236,20 @@
 		isFunction: function(fn) {
 			return (typeof fn === "function");
 		},
+		isElement: function(obj) {
+			try {
+				//Using W3 DOM2 (works for FF, Opera and Chrom)
+				return obj instanceof HTMLElement;
+			} catch(e) {
+				//Browsers not supporting W3 DOM2 don't have HTMLElement and
+				//an exception is thrown and we end up here. Testing some
+				//properties that all elements have. (works on IE7)
+				return (typeof obj === "object") &&
+					(obj.nodeType === 1) &&
+					(typeof obj.style === "object") &&
+					(typeof obj.ownerDocument === "object");
+			}
+		},
 		hasHalfKana: function(str) {
 			if( !this.isString(str) ) {
 				this.cnWarn("hasHalfKana", "arguments[0] must be string.");
